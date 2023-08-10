012ee56abec20a8b0de86f33885d253876e14c58
Kyungjin Lee <105157765+kyungjinleelee@users.noreply.github.com>
2023-08-10 16:32:40 +0900

    Add files via upload

---
+++ exam07_toString/Dog.java
@@ -0,0 +1,47 @@
+package exam07_toString;
+
+public class Dog extends Pet{
+
+	String sex;
+
+	@Override
+	public void eat() {
+		System.out.println("Dog.eat");
+	}
+
+	@Override
+	public void sleep() {
+		System.out.println("Dog.sleep");
+	}
+
+	// sex에 해당되는 생성자, getter setter 만들기 
+	public Dog() {}
+
+	public Dog(String name, int age) {
+		super(name, age);
+	}
+
+	public Dog(String name, int age, String sex) { // 성별까지 포함한 generate 만들기
+		super(name, age);
+		this.sex = sex;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Dog 이름: %s, Dog 나이:%d, Dog 성별:%s \n", name, age, sex);
+	}
+	
+
+	
+	
+	
+	
+}
